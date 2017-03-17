ed80ca38a3f861d8e5d5bc00215f083e005106cb
wangYalun <326402399@qq.com>
2017-03-17 18:14:54 +0800

    test&ignore logs

--- routers/api_test.js
+++ routers/api_test.js
@@ -4,11 +4,22 @@
 
 var express = require('express');
 
+var fs=require('fs');
+
 var router = express.Router();
 
 router.get('/',function(req,res){
     var params=req.query||req.params;
     res.json(params);
+});
+
+router.get('/get_js',function(req,res){
+    fs.readFile('api_test.js',function(err){
+        if(err){
+
+        };
+        res.sendFile('routers/api_test.js');
+    });
 });
 
 module.exports=router;
