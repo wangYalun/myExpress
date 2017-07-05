cc2e710f4393f38131f552d4715d44e61a499343
王雅伦 <wangyl@udiannet.com>
2017-07-05 20:19:10 +0800

    login

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     "default": {
-        host: '119.29.146.193',
-        user: 'allen',
-        password: 'Allen#326402399',
+        host: '39.108.231.153',
+        user: 'allen_remote',
+        password: 'allen123',
         database: 'db_repository_1', // 前面建的user表位于这个数据库中
         port: 3306
     },
---
+++ routers/api/api_login.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var router = express.Router();
+
+var apiLogin = require('../../controllers/api_login');
+
+router.post('/login', function (req, res) {
+    apiLogin.login(req, res);
+});
+
+module.exports = router;
